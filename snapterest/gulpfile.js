2846a7aa078176b28ccae47cc7645cdffc8314ad
deepak6sp <deepak6sp@gmail.com>
2016-01-21 22:05:26 +1100

    master changes

--- snapterest/gulpfile.js
+++ snapterest/gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require("gulp");
+var browserify = require("browserify");
+var babelify = require("babelify");
+var vinylStream =  require("vinyl-source-stream");
 
 gulp.task("default",function(){
-	console.log("i am running gulp");
-
+	return browserify("./source/app.js")
+	.transform(babelify)
+	.bundle()
+	.pipe(vinylStream("snapterest.js"))
+	.pipe(gulp.dest("./build/"));
 });
--- snapterest/source/app.js
+++ snapterest/source/app.js
@@ -1,1 +1,20 @@
-/* entry point */
+var React = require("react");
+var ReactDom =  require("react-dom");
+
+var SomeReact = React.createClass({
+	getInitialState:function(){
+		return {
+			isHeader : false,
+			title:"my only first application"
+		};
+		
+	},
+	render :function(){
+		return <div> 
+					{this.state.title}
+				</div>
+	}
+
+});
+
+React.render(<SomeReact />, document.getElementById("react-application"));
