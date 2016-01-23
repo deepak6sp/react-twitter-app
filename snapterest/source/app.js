7ad58ba4674212dfac8d652cfed82e25905a43cf
deepak6sp <deepak6sp@gmail.com>
2016-01-23 14:20:56 +1100

    header display done

--- snapterest/source/app.js
+++ snapterest/source/app.js
@@ -1,20 +1,38 @@
 var React = require("react");
 var ReactDom =  require("react-dom");
 
+
 var SomeReact = React.createClass({
+
+
 	getInitialState:function(){
 		return {
-			isHeader : false,
-			title:"my only first application"
+			isHeader : true,
+			title:"my only first application",
+			count:0,
+			divDisplay:{display:"block"}
 		};
-		
+	},
+	changeHandle: function(){
+		this.setState({
+			isHeader : !this.state.isHeader
+			//divDisplay:{display:"none"}
+		});
 	},
 	render :function(){
+		if(!this.state.isHeader){
+			this.state.divDisplay.display = "none"
+		}
+		else{
+			this.state.divDisplay.display = "block"
+		}
 		return <div> 
-					{this.state.title}
+					<div style={this.state.divDisplay}>{this.state.title}</div>
+					{this.state.count}
+					<button onClick={this.changeHandle} >click to count</button>
 				</div>
-	}
+	} 
 
 });
 
-React.render(<SomeReact />, document.getElementById("react-application"));
+ReactDom.render(<SomeReact />, document.getElementById("react-application"));
